84915a635c22fd138060336fb760e7d0668f95b3
leninchakr <leninchakra@gmail.com>
2026-06-27 19:15:42 +0530

    First Commit

---
+++ src/main/java/com/example/dockerTest/DockerTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.dockerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DockerTestApplication.class, args);
+
+		System.out.println("\n\nDocker-Spring-Boot Runs Successfully...\n\n");
+	}
+
+}
---
+++ src/main/java/com/example/dockerTest/Test_Controller.java
@@ -0,0 +1,21 @@
+package com.example.dockerTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Test_Controller {
+
+	@GetMapping("/test/1")
+	public String test_1() {
+		return "Test-1";
+	}
+
+	@GetMapping("/test/2")
+	public String test_2() {
+		return "Test-2";
+	}
+
+}
